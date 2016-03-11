77791cd7c5c502ff3cd8d844aae10d7f0ea1ac7a
GraceJonn123 <gj.gjonn@gmail.com>
2016-03-11 04:53:30 +0300

    initial commit

---
+++ app/src/main/java/com/jijjy/grace/kari/Splash.java
@@ -0,0 +1,31 @@
+package com.jijjy.grace.kari;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import java.util.logging.Handler;
+
+public class Splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        int timer = 3000;
+
+        new android.os.Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(getBaseContext(), Main.class));
+            }
+        }, timer);
+    }
+    @Override
+    protected void onStop() {
+        super.onStop();
+        finish();
+    }
+
+}
